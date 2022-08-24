408daf1c4005c21251fec1147c1d82da88633c87
Sibusiso <sibusisom@glucode.com>
2022-08-24 15:00:24 +0200

    Complete core functionality

--- ToDoApp/Controllers/ArchiveCell.swift
+++ ToDoApp/Controllers/ArchiveCell.swift
@@ -12,6 +12,7 @@ class ArchiveCell: UITableViewCell {
     @IBOutlet var taskLabel: UILabel!
     @IBOutlet var taskImage: UIImageView!
     @IBOutlet var taskView: UIView!
+    @IBOutlet var taskTextView: UIView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,6 +23,9 @@ class ArchiveCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         taskImage.tintColor = UIColor(red: 0.96, green: 0.78, blue: 0.92, alpha: 1.0)
+        taskView.layer.cornerRadius = taskView.frame.size.height / 5
+        taskView.backgroundColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.00)
+        taskTextView.backgroundColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.00)
 
     }
 
--- ToDoApp/Controllers/TaskCell.swift
+++ ToDoApp/Controllers/TaskCell.swift
@@ -13,16 +13,24 @@ class TaskCell: UITableViewCell {
     @IBOutlet var dateTaskLabel: UILabel!
     @IBOutlet var taskImage: UIImageView!
     @IBOutlet var taskView: UIView!
+    @IBOutlet var taskTextView: UIView!
 
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        taskImage.tintColor = UIColor(red: 0.88, green: 0.94, blue: 0.92, alpha: 1.0)
+        taskView.layer.cornerRadius = taskView.frame.size.height / 5
+        taskView.backgroundColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.00)
+        taskTextView.backgroundColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.00)
+
+//        taskView.backgroundColor = UIColor(red: 0.98, green: 0.96, blue: 0.72, alpha: 1.00)
+
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        taskImage.tintColor = UIColor(red: 0.88, green: 0.94, blue: 0.92, alpha: 1.0)
     }
 
 }
